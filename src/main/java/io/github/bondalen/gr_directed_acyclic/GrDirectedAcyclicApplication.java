0c30e27ceb2216d3fd569a70301ead453b1c2944
bondalen <jjjalexbond@gmail.com>
2025-06-18 21:49:02 +0300

    start

---
+++ src/main/java/io/github/bondalen/gr_directed_acyclic/GrDirectedAcyclicApplication.java
@@ -0,0 +1,13 @@
+package io.github.bondalen.gr_directed_acyclic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrDirectedAcyclicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrDirectedAcyclicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/bondalen/gr_directed_acyclic/GrDirectedAcyclicApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bondalen.gr_directed_acyclic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrDirectedAcyclicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
