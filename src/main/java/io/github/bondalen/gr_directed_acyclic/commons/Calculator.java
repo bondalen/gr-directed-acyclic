877191b46bf8c7817ff1d409d046faee74ae57f9
bondalen <jjjalexbond@gmail.com>
2025-06-19 11:51:37 +0300

    calculator, java 21

---
+++ src/main/java/io/github/bondalen/gr_directed_acyclic/commons/Calculator.java
@@ -0,0 +1,14 @@
+package io.github.bondalen.gr_directed_acyclic.commons;
+
+public class Calculator {
+    
+    // конструктор
+    public Calculator(){
+
+    }
+
+    public Long calc(long num1, long num2){
+        return num1 + num2;
+    }
+
+}
